f65eed26235315a19aecb6ea2403650b3f664230
HenryLoenwind <henry@loenwind.info>
2015-11-01 23:11:13 +0100

    Protect against anchors being painted as themselves

--- src/main/java/crazypants/enderio/teleport/anchor/BlockTravelAnchor.java
+++ src/main/java/crazypants/enderio/teleport/anchor/BlockTravelAnchor.java
@@ -98,7 +98,7 @@ public class BlockTravelAnchor extends BlockEio implements IGuiHandler, ITileEnt
     TileEntity te = world.getTileEntity(x, y, z);
     if (te instanceof IPaintableTileEntity) {
       Block sourceBlock = ((IPaintableTileEntity) te).getSourceBlock();
-      if (sourceBlock != null) {
+      if (sourceBlock != null && sourceBlock != this) {
         return sourceBlock.getIcon(blockSide, ((IPaintableTileEntity) te).getSourceBlockMetadata());
       }
     }
@@ -200,7 +200,7 @@ public class BlockTravelAnchor extends BlockEio implements IGuiHandler, ITileEnt
     TileEntity te = world.getTileEntity(x, y, z);
     if (te instanceof IPaintableTileEntity) {
       Block sourceBlock = ((IPaintableTileEntity) te).getSourceBlock();
-      if (sourceBlock != null) {
+      if (sourceBlock != null && sourceBlock != this) {
         return sourceBlock.colorMultiplier(world, x, y, z);
       }
     }
